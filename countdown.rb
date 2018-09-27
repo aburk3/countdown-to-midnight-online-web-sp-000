de13b07fda4bf7c6adbf7191c8b21ef0cd80bb41
Austin  Burke <aburk3@gmail.com>
2018-09-27 18:30:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0 
     puts "#{seconds_to_midnight} SECOND(S)!"
-    number -= 1
+    seconds_to_midnight -= 1
   end
   return "HAPPY NEW YEAR!"
 end
